37cf93a528b0d3275ff46627a7327e3ac739f3e1
martynamielczarek <m.martynamielczarek@gmail.com>
2020-09-24 20:09:42 +0200

    -

---
+++ src/main/java/com/mm/MmApplication.java
@@ -0,0 +1,22 @@
+package com.mm;
+
+//import com.mm.entity.Activity;
+//import com.mm.repositories.ActivityRepository;
+//import com.mm.services.ActivityService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class MmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmApplication.class, args);
+	}
+
+
+
+}
